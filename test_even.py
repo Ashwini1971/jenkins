a85532882fae332ea2d494bc5aaa7cbb61325ea7
selenium project <sashwini.2021@gmail.com>
2024-12-22 17:15:16 +0530

    test_even modified

--- test_even.py
+++ test_even.py
@@ -6,3 +6,11 @@ def test_is_even():
 
 
 
+'''
+http://localhost:8081/github-webhook --> the url is private / load
+        6. To make the url public -->
+            1. download and unzip ngrok
+            2. ngrok config add-authtoken $YOUR_AUTHTOKEN
+            3. go to ngrok location, open command prompt, enter ngrok http http://localhost:8081
+            4. we will get the https url --> demo (https://75af-27-59-96-121.ngrok-free.app/github-webhook)
+'''
